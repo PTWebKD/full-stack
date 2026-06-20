d306cc756c3a4925cc713da94fb41ab7d40b87eb
PTWebKD <phamtranthanhtai7@gmail.com>
2026-06-20 16:26:21 +0700

    fix: correct alembic prepend_sys_path to parent directory

--- BE/alembic/env.py
+++ BE/alembic/env.py
@@ -1,11 +1,7 @@
 import asyncio
-import sys
-from pathlib import Path
 from logging.config import fileConfig
 from sqlalchemy.ext.asyncio import create_async_engine
 from alembic import context
-
-sys.path.insert(0, str(Path(__file__).parent.parent))
 
 config = context.config
 
